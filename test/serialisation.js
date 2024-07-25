5fcdc0a6476dd68dbee8366d6b088b808a55895f
Liam Le Brun <liam@event.engineering>
2024-07-25 09:38:44 +0100

    The start to some serialisation functions

---
+++ test/serialisation.js
@@ -0,0 +1,13 @@
+import ServiceObj from '../lib/service-obj.js';
+
+let original = new ServiceObj({name: 'steve'});
+
+let a1 = structuredClone(original);
+let b1 = {...original};
+let c1 = Object.entries(original);
+
+let a2 = structuredClone(original.toObject());
+let b2 = {...original.toObject()};
+let c2 = Object.entries(original.toObject());
+
+console.debug('What have we got?', {original, jsonCloned: JSON.parse(JSON.stringify(original)), fromOriginal: {a1, b1, c1}, fromToObjected: {a2, b2, c2}, string: original.toString()});
