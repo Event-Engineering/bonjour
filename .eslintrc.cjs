c5193a2bf1a8077f8f3a01200aff826fd0b71196
Liam Le Brun <liam@event.engineering>
2024-07-25 15:23:32 +0100

    Updated rules and fix the script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,9 +16,9 @@ module.exports = {
 	},
 	rules: {
 		curly: 'error',
-		'comma-dangle': ['error', 'always-multiline'],
+		'comma-dangle': ['error', {arrays: 'always-multiline', objects: 'always-multiline', imports: 'always-multiline', exports: 'always-multiline', functions: 'never'}],
 		semi: 'error',
-		indent: ['error', 'tab', {VariableDeclarator: 0, SwitchCase: 1, MemberExpression: 0,}],
+		indent: ['error', 'tab', {VariableDeclarator: 0, SwitchCase: 1, MemberExpression: 0}],
 		'no-unused-vars': ['warn', {vars: 'all', args: 'none', ignoreRestSiblings: false }],
 		'no-fallthrough': 'off',
 		'no-inner-declarations': 'off',
